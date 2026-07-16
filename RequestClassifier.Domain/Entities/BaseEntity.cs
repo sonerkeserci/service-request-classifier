5d9f209bddc1ab8ca3a039d9433be44396649a3c
sonerkeserci <sonerkeserci@gmail.com>
2026-07-16 13:18:22 +0300

    Add project files.

---
+++ RequestClassifier.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestClassifier.Domain.Entities
+{
+    internal class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
