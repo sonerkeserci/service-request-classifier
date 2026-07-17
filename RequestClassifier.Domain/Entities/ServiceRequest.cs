ddd7c3eadaa85df037e3a06f9ad9408f66beabf2
sonerkeserci <sonerkeserci@gmail.com>
2026-07-17 09:06:00 +0300

    .

--- RequestClassifier.Domain/Entities/ServiceRequest.cs
+++ RequestClassifier.Domain/Entities/ServiceRequest.cs
@@ -16,13 +16,17 @@ namespace RequestClassifier.Domain.Entities
         public string Description { get; set; } = string.Empty;
         public RequestStatus Status { get; set; } = RequestStatus.Received;
 
+
+
+        public float? PredictionScore { get; set; }     // Confidence score of the predicted category
+
+
         public int? PredictedCategoryId { get; set; }
         public RequestCategory? PredictedCategory { get; set; } // Category predicted by the system's machine learning model
 
         public int? AssignedCategoryId { get; set; }
         public RequestCategory? AssignedCategory { get; set; }  // Category assigned by the system or personnel after reviewing the prediction
 
-        public float? PredictionScore { get; set; }     // Confidence score of the predicted category
 
         public bool IsAutoAssigned { get; set; }
         public ICollection<RequestStatusHistory> StatusHistories { get; set; } = new List<RequestStatusHistory>(); // History of status changes for the request
