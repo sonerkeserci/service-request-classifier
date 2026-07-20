d6f34b723e441e4ace009fca9dfc3e133840870d
sonerkeserci <sonerkeserci@gmail.com>
2026-07-20 23:06:01 +0300

    Add IAuthService interface with auth methods

---
+++ RequestClassifier.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using RequestClassifier.Application.DTOs.Auth;
+
+namespace RequestClassifier.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto?> LoginAsync(LoginDto dto); // Returns null if login fails; returns AuthResponseDto if login is successful
+
+    Task<bool> CreateEmployeeAsync(CreateEmployeeDto dto);  // Returns true if employee creation is successful
+}
