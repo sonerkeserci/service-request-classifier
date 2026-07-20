69c27d7cf0a97513aec7762ff4a119ab5185c392
sonerkeserci <sonerkeserci@gmail.com>
2026-07-20 23:03:07 +0300

    Add IRequestCategoryService interface for category CRUD

---
+++ RequestClassifier.Application/Interfaces/IRequestCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using RequestClassifier.Application.DTOs.RequestCategories;
+
+namespace RequestClassifier.Application.Interfaces;
+
+public interface IRequestCategoryService
+{
+    Task<List<RequestCategoryDto>> GetAllAsync();
+
+    Task<RequestCategoryDto?> GetByIdAsync(int id);
+
+    Task<List<RequestCategoryDto>> GetByDepartmentIdAsync(int departmentId); // Get all request categories for a specific department
+
+    Task<RequestCategoryDto> CreateAsync(CreateRequestCategoryDto dto);
+
+    Task<bool> UpdateAsync(int id, UpdateRequestCategoryDto dto);
+}
