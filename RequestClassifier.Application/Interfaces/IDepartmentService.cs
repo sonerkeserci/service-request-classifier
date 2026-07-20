1083e44ac42a7e3e8f23966d74799f593a15d47a
sonerkeserci <sonerkeserci@gmail.com>
2026-07-20 22:21:47 +0300

    Add IDepartmentService and IServiceRequestService interfaces

---
+++ RequestClassifier.Application/Interfaces/IDepartmentService.cs
@@ -0,0 +1,14 @@
+﻿using RequestClassifier.Application.DTOs.Departments;
+
+namespace RequestClassifier.Application.Interfaces;
+
+public interface IDepartmentService
+{
+    Task<List<DepartmentDto>> GetAllAsync();
+
+    Task<DepartmentDto?> GetByIdAsync(int id);
+
+    Task<DepartmentDto> CreateAsync(CreateDepartmentDto dto);
+
+    Task<bool> UpdateAsync(int id, UpdateDepartmentDto dto);
+}
---
+++ RequestClassifier.Application/Interfaces/IServiceRequestService.cs
@@ -0,0 +1,16 @@
+﻿using RequestClassifier.Application.DTOs.ServiceRequests;
+
+namespace RequestClassifier.Application.Interfaces;
+
+public interface IServiceRequestService
+{
+    Task<ServiceRequestDetailDto> CreateAsync(CreateServiceRequestDto dto); // Input: CreateServiceRequestDto, Output: ServiceRequestDetailDto
+
+    Task<ServiceRequestDetailDto?> TrackAsync(TrackServiceRequestDto dto);
+
+    Task<ServiceRequestDetailDto?> GetByIdAsync(int id);    // Input: id, Output: ServiceRequestDetailDto
+
+    Task<List<ServiceRequestDetailDto>> GetAllAsync();
+
+    Task<bool> UpdateStatusAsync(int id, UpdateRequestStatusDto dto);   // Input: id, UpdateRequestStatusDto, Output: bool (true if successful, false if not)
+}
