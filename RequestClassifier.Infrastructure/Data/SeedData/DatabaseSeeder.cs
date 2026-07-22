65f85a1c9beddd166db9adf7fb7d0335fde2cc58
sonerkeserci <sonerkeserci@gmail.com>
2026-07-22 08:37:08 +0300

    Add DatabaseSeeder for initial data seeding
    Introduced a static DatabaseSeeder class with an async SeedAsync method in the RequestClassifier.Infrastructure.Data.Seed namespace. The seeder checks for existing departments and includes placeholders for seeding Departments and RequestCategories. Designed to be invoked once from Program.cs.

---
+++ RequestClassifier.Infrastructure/Data/SeedData/DatabaseSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestClassifier.Infrastructure.Data;
+
+namespace RequestClassifier.Infrastructure.Data.Seed;
+
+public static class DatabaseSeeder
+{
+    public static async Task SeedAsync(ApplicationDbContext context)
+    {
+        if (await context.Departments.AnyAsync()) // If any data has added, return
+            return;
+
+        /*
+        Departments and RequestCategories will be added here.
+        This method will be called once in Program.cs, then disabled.
+        */
+    }
+}
