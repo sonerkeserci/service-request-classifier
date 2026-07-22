98bd1595e02e55e3a63c9cb557aa0b32542bdad0
sonerkeserci <sonerkeserci@gmail.com>
2026-07-22 15:58:04 +0300

    Add RequestStatusHistoryDto class for status tracking

---
+++ RequestClassifier.Application/DTOs/ServiceRequests/RequestStatusHistoryDto.cs
@@ -0,0 +1,16 @@
+﻿using RequestClassifier.Domain.Enums;
+
+namespace RequestClassifier.Application.DTOs.ServiceRequests;
+
+public class RequestStatusHistoryDto
+{
+    public int Id { get; set; }
+
+    public RequestStatus? OldStatus { get; set; }
+
+    public RequestStatus NewStatus { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime ChangedAt { get; set; }
+}
