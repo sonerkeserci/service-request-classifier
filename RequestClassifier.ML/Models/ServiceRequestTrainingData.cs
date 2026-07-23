a83e833a3c570a669fec65d569ca0785f186f33c
sonerkeserci <sonerkeserci@gmail.com>
2026-07-23 14:41:40 +0300

    Enable ML.NET data mapping with [LoadColumn] attributes

--- RequestClassifier.ML/Models/ServiceRequestTrainingData.cs
+++ RequestClassifier.ML/Models/ServiceRequestTrainingData.cs
@@ -1,7 +1,11 @@
-﻿namespace RequestClassifier.ML.Models;
+﻿using Microsoft.ML.Data;
+
+namespace RequestClassifier.ML.Models;
 
 public class ServiceRequesTrainingData      // Represents the input text and its correct category used to train the model.
 {
+    [LoadColumn(0)]
     public string Text { get; set; } = string.Empty;
+    [LoadColumn(1)]
     public string Category { get; set; } = string.Empty;
 }
