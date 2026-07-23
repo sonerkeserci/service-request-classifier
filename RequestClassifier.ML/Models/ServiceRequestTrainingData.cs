897d845730681faed2a08eb78709d35910c0c3a7
sonerkeserci <sonerkeserci@gmail.com>
2026-07-23 14:56:24 +0300

    Fix typo in ServiceRequestTrainingData class name

--- RequestClassifier.ML/Models/ServiceRequestTrainingData.cs
+++ RequestClassifier.ML/Models/ServiceRequestTrainingData.cs
@@ -2,7 +2,7 @@
 
 namespace RequestClassifier.ML.Models;
 
-public class ServiceRequesTrainingData      // Represents the input text and its correct category used to train the model.
+public class ServiceRequestTrainingData      // Represents the input text and its correct category used to train the model.
 {
     [LoadColumn(0)]
     public string Text { get; set; } = string.Empty;
