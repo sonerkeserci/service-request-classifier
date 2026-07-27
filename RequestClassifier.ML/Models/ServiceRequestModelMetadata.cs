94b82df1446d1e9855530027bb1e05668b8bb879
sonerkeserci <sonerkeserci@gmail.com>
2026-07-27 16:16:34 +0300

    Add ServiceRequestModelMetadata for ML model metadata
    Added RequestClassifier.ML.Models namespace with ServiceRequestModelMetadata class to store category names from a trained ML.NET model's Score vector.

---
+++ RequestClassifier.ML/Models/ServiceRequestModelMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace RequestClassifier.ML.Models;
+
+// Contains metadata extracted from the trained ML.NET model.
+public class ServiceRequestModelMetadata
+{
+    // Contains category names in the same order as the model's Score vector.
+    public IReadOnlyList<string> CategoryNames { get; init; } = [];
+}
