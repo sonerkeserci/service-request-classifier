38b0300f3b2c89f127daaf6a6abe8d0fce16ee88
sonerkeserci <sonerkeserci@gmail.com>
2026-07-30 14:57:23 +0300

    Add HomeController class

--- RequestClassifier.Web/Controllers/HomeController.cs
+++ RequestClassifier.Web/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace RequestClassifier.Web.Controllers
+namespace RequestClassifier.Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
     }
 }
