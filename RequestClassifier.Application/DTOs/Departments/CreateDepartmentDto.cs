31ca2c1fd4afa47f6164dc71e201d5b6163ada07
sonerkeserci <sonerkeserci@gmail.com>
2026-08-02 01:45:18 +0300

    Add validation to Department DTOs; remove IsActive from Update
    Added data annotation attributes to Name, Code, and Description in CreateDepartmentDto and UpdateDepartmentDto for validation. Included necessary using directives. Removed IsActive from UpdateDepartmentDto.

--- RequestClassifier.Application/DTOs/Departments/CreateDepartmentDto.cs
+++ RequestClassifier.Application/DTOs/Departments/CreateDepartmentDto.cs
@@ -1,11 +1,18 @@
-﻿namespace RequestClassifier.Application.DTOs.Departments;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequestClassifier.Application.DTOs.Departments;
 
 // This DTO is used for creating a new department by administrator.
 public class CreateDepartmentDto
 {
+    [Required]
+    [StringLength(150)]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(5)]
     public string Code { get; set; } = string.Empty;
 
+    [StringLength(250)] 
     public string? Description { get; set; }
 }
--- RequestClassifier.Application/DTOs/Departments/UpdateDepartmentDto.cs
+++ RequestClassifier.Application/DTOs/Departments/UpdateDepartmentDto.cs
@@ -1,13 +1,19 @@
-﻿namespace RequestClassifier.Application.DTOs.Departments;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequestClassifier.Application.DTOs.Departments;
 
 // This DTO is used for updating an existing department by administrator.
 public class UpdateDepartmentDto
 {
+    [Required]
+    [StringLength(150)]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(5)]
     public string Code { get; set; } = string.Empty;
 
+    [StringLength(250)]
     public string? Description { get; set; }
-
     public bool IsActive { get; set; }
 }
